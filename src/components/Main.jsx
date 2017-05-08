c66edc8cb4cf112a63bb07b5912d0a6b8df9322e
CHINAN CHENG <CHINAN CHENG>
2017-05-08 23:34:12 +0800

    initial-version by chinan

---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Main.css';
+
+export default class Main extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+      return(
+        <h1>Hello World!! by handsome man</h1>
+      );
+    }
+}
